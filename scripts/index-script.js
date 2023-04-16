ca498311e55d5cf57a50aff6dc14e8bf707e3788
Immanuel-l <immanuel.leinert1@gmail.com>
2023-04-16 14:50:19 +0200

    API auf Deutsch gesetzt

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -5,7 +5,7 @@
 
     const API_KEY = "api_key=b91afbd28e2bdc66c587c96ade03deb9";
     const BASE_URL = "https://api.themoviedb.org/3";
-    const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+    const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY + "&language=de-DE";
     const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
     function getMovies(url) {
--- scripts/result.js
+++ scripts/result.js
@@ -11,14 +11,14 @@ const overview = document.querySelector(".overview");
 
 const API_KEY = "api_key=b91afbd28e2bdc66c587c96ade03deb9";
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + "/movie/" + id + "?" + API_KEY;
+const API_URL = BASE_URL + "/movie/" + id + "?" + API_KEY + "&language=de-DE";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 console.log(API_URL);
 
 function getMovie(url) {
     fetch(url).then(res => res.json()).then(data => {
-        let title = data.original_title;
+        let title = data.title;
         document.title = title;
         movie_title.innerHTML = title;
         poster.src = IMG_URL + data.poster_path;
